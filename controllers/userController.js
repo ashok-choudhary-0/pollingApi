b6a48ea21b3c5881d2a77bd669095602fb8b3598
ashok-choudhary-0 <ashok.kumar@innotechteam.in>
2023-09-28 18:22:09 +0530

    remvove updated password

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,10 +34,10 @@ const deleteSingleUser = async (req, res) => {
   }
 }
 const updateSingleUser = async (req, res) => {
-  const { firstName, lastName, password } = req.body;
+  const { firstName, lastName } = req.body;
   const userId = req.params.id
   try {
-    const updateDatabaseUser = await userModel.update({ firstName, lastName, password }, { where: { id: userId } })
+    const updateDatabaseUser = await userModel.update({ firstName, lastName }, { where: { id: userId } })
     if (updateDatabaseUser[0]) {
       res.status(200).send({ message: "User updated successfully" })
     } else {
