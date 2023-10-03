7f89b833a88db609195207ef0dbc9005e3e0dd3d
ashok-choudhary-0 <ashok.kumar@innotechteam.in>
2023-10-03 14:16:55 +0530

    remove comments

--- index.js
+++ index.js
@@ -11,11 +11,6 @@ app.use("/", authenticationRouter)
 app.use("/user", userRouter);
 app.use("/poll", pollRouter)
 
-// const vote = require("./models/voteModel");
-// vote.sync({ alter: true });
-// const option = require("./models/optionsModel")
-// option.sync({ alter: true })
-
 const PORT = process.env.port || 3000;
 app.listen(PORT, (req, res) => {
   console.log(`server is running on port ${PORT}`)
